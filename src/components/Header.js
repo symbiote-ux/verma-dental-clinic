476d1cbca3184e05cb51d8d3825264376632d4d6
Saurabh Chauhan <tnikhil422@gmail.com>
2024-09-10 13:27:21 +0530

    Make footer responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,14 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 bg-[#FFF7F0] shadow-md z-50 text-[#1D1D1D]">
+    <header className="sticky top-0 bg-[#FFF7F0] z-50 text-[#1D1D1D]">
       <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
         <div className="flex items-center space-x-2">
           <Image
             src="/images/smileschangeslives-logo.png"
             alt="smiles changes lives logo"
-            width={40}
-            height={40}
+            width={50}
+            height={50}
           />
           <h1 className="text-lg font-bold">
             Dr. Verma’s Dental Surgery & Braces Centre
@@ -27,35 +27,38 @@ export default function Header() {
             onClick={() => setMenuOpen(!menuOpen)}
             className="focus:outline-none"
           >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
+            {menuOpen ? (
+              '✖'
+            ) : (
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              </svg>
+            )}
           </button>
         </div>
 
         {/* Navbar Links - visible on large screens */}
         <nav className="hidden lg:flex space-x-4 items-center">
-          <Link href="/about" className="text-gray-700 hover:text-blue-600">
+          <Link href="/about" className="hover:text-[#18A2BB] font-semibold">
             About
           </Link>
-          <Link href="/services" className="text-gray-700 hover:text-blue-600">
+          <Link href="/services" className="font-semibold hover:text-[#18A2BB]">
             Services
           </Link>
-
-          <Link href={`tel:${'+918037296355'}`}>
-            <button className="flex items-center text-lg justify-center text-white bg-[#18A2BB] rounded-xl border-2 border-transparent p-2 hover:bg-white hover:text-[#18A2BB] hover:border-[#18A2BB] transition-all duration-300">
-              +918037296355
+          <Link href={`tel:${'075002 25225'}`}>
+            <button className="flex items-center text-lg justify-center text-white  font-semibold bg-[#18A2BB] rounded-3xl border-2 border-transparent p-2 px-4 hover:bg-white hover:text-[#18A2BB] hover:border-[#18A2BB] transition-all duration-300">
+              075002 25225
             </button>
           </Link>
         </nav>
@@ -63,25 +66,22 @@ export default function Header() {
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <nav className="lg:hidden bg-white shadow-md">
-          <ul className="flex flex-col space-y-2 p-4">
+        <nav className="lg:hidden bg-[#18A2BB] shadow-md">
+          <ul className="flex flex-col space-y-2">
             <li>
-              <Link href="/about" className="text-gray-700 hover:text-blue-600">
+              <Link href="/about" className=" text-white font-semibold px-2">
                 About
               </Link>
             </li>
             <li>
-              <Link
-                href="/services"
-                className="text-gray-700 hover:text-blue-600"
-              >
+              <Link href="/services" className="text-white font-semibold p-2">
                 Services
               </Link>
             </li>
             <li>
-              <Link href={`tel:${'+918037296355'}`}>
-                <button className="flex items-center justify-center text-white bg-[#18A2BB] p-2 rounded-xl border-2 border-transparent hover:bg-white text-lg hover:text-[#18A2BB] hover:border-[#18A2BB] transition-all duration-300">
-                  +918037296355
+              <Link href={`tel:${'075002 25225'}`}>
+                <button className="flex items-center justify-start text-[#18A2BB] bg-white  font-semibold p-2 border-2 w-full border-transparent hover:bg-[#18A2BB] text-lg hover:text-white transition-all duration-300">
+                  075002 25225
                 </button>
               </Link>
             </li>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,31 +3,35 @@ import Link from 'next/link';
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center h-screen max-w-7xl mx-auto px-4 space-y-6 md:space-y-0 md:space-x-8">
-      <div className="flex-1 text-center md:text-left">
-        <h1 className="text-3xl md:text-4xl font-bold mb-4">
-          Relaxed office. Experienced team. Exceptional care.
-        </h1>
-        <h2 className="text-xl md:text-2xl text-gray-600 mb-4">
-          Trusted Kansas City Dentist <br />
-          Serving the Hawthorne – Picture Hills area
-        </h2>
-        <Link href={`tel:${'+918037296355'}`}>
-          <button className="flex items-center justify-center text-white bg-[#18A2BB] p-2 rounded-xl border-2 border-transparent hover:bg-white text-lg hover:text-[#18A2BB] hover:border-[#18A2BB] transition-all duration-300">
-            +918037296355
-          </button>
-        </Link>
-      </div>
+    <div className="bg-[#FFF7F0]">
+      <div className="flex flex-col md:flex-row items-center justify-center h-screen max-w-7xl  mx-auto px-4 space-y-6 md:space-y-0 md:space-x-8">
+        <div className="flex-1 text-center md:text-left">
+          <h2 className="text-xl text-[#18A2BB] md:text-2xl text-left font-bold mb-4">
+            Relaxed office. Experienced team. Exceptional care.
+          </h2>
+          <h1 className="text-5xl font-bold text-left  md:text-6xl mb-4">
+            Trusted Dehradun City Dentist
+          </h1>
+          <h2 className="text-xl text-[#18A2BB] text-left md:text-2xl font-bold mb-4">
+            Serving the Dehradun – Haridwar, Rishikesh Area.
+          </h2>
+          <Link href={`tel:${'075002 25225'}`}>
+            <button className="flex items-center justify-center text-white bg-[#18A2BB] p-2 px-4 font-semibold  md:mt-6 rounded-3xl border-2 border-transparent hover:bg-white text-lg hover:text-[#18A2BB] hover:border-[#18A2BB] transition-all duration-300">
+              075002 25225
+            </button>
+          </Link>
+        </div>
 
-      {/* Image Section */}
-      <div className="flex-1">
-        <Image
-          src="/images/front-view.jpg" // Replace with your image path
-          alt="Dental Clinic Team"
-          width={650}
-          height={500}
-          className="rounded-lg object-cover"
-        />
+        {/* Image Section */}
+        <div className="flex-1">
+          <Image
+            src="/images/front-view.jpg"
+            alt="Dental Clinic reception image"
+            width={650}
+            height={500}
+            className="rounded-lg object-cover"
+          />
+        </div>
       </div>
     </div>
   );
